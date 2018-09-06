6a87a6d7fac8ef80176e4b30b3d5ef8c1adbfce4
Philipp Harth <harth@zib.de>
2018-09-06 15:15:15 +0200

    Network Simulator v8

--- src/cudaVS/Simulator/kernel.cu
+++ src/cudaVS/Simulator/kernel.cu
@@ -1,6 +1,7 @@
 
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
+#include <curand_kernel.h>
 
 #include <stdio.h>
 #include <map>
@@ -12,6 +13,8 @@
 #include <fstream>
 #include <ctime>
 #include <random>
+#include <omp.h>
+#include <fstream>
 
 /*
 #####################################################################################
@@ -120,19 +123,66 @@ void flattenFeatures(std::map<int, std::map<int, float> >& neuron_voxel_pre,
 	}
 }
 
-__global__ void addKernel(int *c, const int *a, const int *b)
+/*
+#####################################################################################
+GPU COMPUTATION
+#####################################################################################
+*/
+
+__global__ void calcKernel(float *contacts,
+	float *pre,
+	float *post,
+	float* postAll,
+	float b0,
+	unsigned int nVoxel,
+	unsigned int nPre,
+	unsigned int nPost
+)
 {
-	int i = threadIdx.x;
-	c[i] = a[i] + b[i];
+	int i = blockIdx.x * blockDim.x + threadIdx.x;
+	int j = blockIdx.y * blockDim.y + threadIdx.y;
+
+	if (i < nPre && j < nPost) {
+		for (unsigned int k = 0; k < nVoxel; k++)
+		{
+			float preVal = pre[i * nVoxel + k];
+			float postVal = post[j * nVoxel + k];
+			if (preVal != 0 && postVal != 0 && contacts[i * nPost + j] < 1000) {
+				float arg = b0 + preVal + postVal + postAll[k];
+				int synapses = 0;
+				if (arg >= -7 && arg <= 7)
+				{
+					float mu = exp(arg);
+					contacts[i * nPost + j] += mu;
+				}
+				else if (arg > 7)
+				{
+					contacts[i * nPost + j] = 1000;
+				}
+			}
+		}
+	}
 }
 
 // Helper function for using CUDA to add vectors in parallel.
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
+cudaError_t calcWithCuda(float *pre,
+	float *post,
+	float *postAll,
+	float b0,
+	unsigned int nPre,
+	unsigned int nPost,
+	unsigned int nVoxel,
+	float* contacts,
+	std::clock_t start,
+	bool verbose)
 {
-	int *dev_a = 0;
-	int *dev_b = 0;
-	int *dev_c = 0;
+	float *dev_pre = 0;
+	float *dev_post = 0;
+	float *dev_postAll = 0;
+	float *dev_contacts = 0;
 	cudaError_t cudaStatus;
+
+	double copyToDeviceStartTime = std::clock();
 
 	// Choose which GPU to run on, change this on a multi-GPU system.
 	cudaStatus = cudaSetDevice(0);
@@ -141,45 +191,78 @@ cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
 		goto Error;
 	}
 
-	// Allocate GPU buffers for three vectors (two input, one output)    .
-	cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
+	cudaStatus = cudaMalloc((void**)&dev_pre, nPre * nVoxel * sizeof(float));
 	if (cudaStatus != cudaSuccess) {
 		fprintf(stderr, "cudaMalloc failed!");
 		goto Error;
 	}
 
-	cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
+	cudaStatus = cudaMalloc((void**)&dev_post, nPost * nVoxel * sizeof(float));
 	if (cudaStatus != cudaSuccess) {
 		fprintf(stderr, "cudaMalloc failed!");
 		goto Error;
 	}
 
-	cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
+	cudaStatus = cudaMalloc((void**)&dev_postAll, nVoxel * sizeof(float));
 	if (cudaStatus != cudaSuccess) {
 		fprintf(stderr, "cudaMalloc failed!");
 		goto Error;
 	}
 
-	// Copy input vectors from host memory to GPU buffers.
-	cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
+	cudaStatus = cudaMalloc((void**)&dev_contacts, nPre * nPost * sizeof(float));
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc failed!");
+		goto Error;
+	}
+
+	cudaStatus = cudaMemcpy(dev_pre, pre, nPre * nVoxel * sizeof(float), cudaMemcpyHostToDevice);
 	if (cudaStatus != cudaSuccess) {
 		fprintf(stderr, "cudaMemcpy failed!");
 		goto Error;
 	}
 
-	cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
+	cudaStatus = cudaMemcpy(dev_post, post, nPost * nVoxel * sizeof(float), cudaMemcpyHostToDevice);
 	if (cudaStatus != cudaSuccess) {
 		fprintf(stderr, "cudaMemcpy failed!");
 		goto Error;
 	}
 
-	// Launch a kernel on the GPU with one thread for each element.
-	//addKernel << <1, size >> > (dev_c, dev_a, dev_b);
+	cudaStatus = cudaMemcpy(dev_postAll, postAll, nVoxel * sizeof(float), cudaMemcpyHostToDevice);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMemcpy failed!");
+		goto Error;
+	}
+
+	cudaStatus = cudaMemcpy(dev_contacts, contacts, nPre * nPost * sizeof(bool), cudaMemcpyHostToDevice);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMemcpy failed!");
+		goto Error;
+	}
+
+	double duration;
+	if (verbose) {
+		duration = (std::clock() - copyToDeviceStartTime) / (double)CLOCKS_PER_SEC;
+		std::cout << "[*] Copy to device " << duration << std::endl;
+	}
+	double computeKernelStartTime = std::clock();
+
+	// Launch a kernel on the GPU
+	dim3 threads(16, 16);
+	dim3 blocks(nPre / threads.x + 1, nPost / threads.y + 1);
+	calcKernel << <blocks, threads >> > (dev_contacts,
+		dev_pre,
+		dev_post,
+		dev_postAll,
+		b0,
+		nVoxel,
+		nPre,
+		nPost
+		);
 
 	// Check for any errors launching the kernel
 	cudaStatus = cudaGetLastError();
 	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
+		fprintf(stderr, "calcKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
 		goto Error;
 	}
 
@@ -191,17 +274,28 @@ cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
 		goto Error;
 	}
 
+	if (verbose) {
+		duration = (std::clock() - computeKernelStartTime) / (double)CLOCKS_PER_SEC;
+		std::cout << "[*] Compute GPU " << duration << std::endl;
+	}
+	double copyToHostTime = std::clock();
+
 	// Copy output vector from GPU buffer to host memory.
-	cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
+	cudaStatus = cudaMemcpy(contacts, dev_contacts, nPre * nPost * sizeof(float), cudaMemcpyDeviceToHost);
 	if (cudaStatus != cudaSuccess) {
 		fprintf(stderr, "cudaMemcpy failed!");
 		goto Error;
 	}
 
+	if (verbose) {
+		duration = (std::clock() - copyToHostTime) / (double)CLOCKS_PER_SEC;
+		std::cout << "[*] Copy to host " << duration << std::endl;
+	}
 Error:
-	cudaFree(dev_c);
-	cudaFree(dev_a);
-	cudaFree(dev_b);
+	cudaFree(dev_pre);
+	cudaFree(dev_post);
+	cudaFree(dev_postAll);
+	cudaFree(dev_contacts);
 
 	return cudaStatus;
 }
@@ -214,20 +308,24 @@ MAIN
 
 int main(int argc, char *argv[]) {
 
-	/*
-	if (argc != 5) {
+	if (argc != 6) {
 		std::cout << "Usage:" << std::endl;
-		std::cout << "Simulator.exe theta1 theta2 theta3 theta4" << std::endl;
+		std::cout << "Simulator.exe CPU|GPU theta1 theta2 theta3 theta4" << std::endl;
 		return -1;
-	}*/
+	}
+
+	std::string mode = argv[1];
+	bool gpu = mode.compare("GPU") == 0;
 
 	float b0, b1, b2, b3;
-	b0 = 0;//std::stof(argv[1]);
-	b1 = 1;// std::stof(argv[2]);
-	b2 = 1;// std::stof(argv[3]);
-	b3 = -1;// std::stof(argv[4]);
+	b0 = std::stof(argv[2]);
+	b1 = std::stof(argv[3]);
+	b2 = std::stof(argv[4]);
+	b3 = std::stof(argv[5]);
 
-	std::cout << "Start simulation" << " " << b0 << " " << b1 << " " << b2 << " " << b3 << std::endl;
+	bool verbose = false;
+
+	std::cout << "[*] Start simulation " << b0 << " " << b1 << " " << b2 << " " << b3 << std::endl;
 	std::clock_t start;
 	double duration;
 	start = std::clock();
@@ -238,120 +336,158 @@ int main(int argc, char *argv[]) {
 
 	std::string baseDir = getBaseDir();
 
-	duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;
-	std::cout << "Start list files pre" << " " << duration << std::endl;
+	double readFeatureTime = std::clock();
 
 	std::vector<std::string> preFiles = getFiles(getDirPath(baseDir, "features_pre"));
-
-	duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;
-	std::cout << "Listed files pre" << " " << duration << std::endl;
-
 	readFields(preFiles, neuron_voxel_pre, b1);
-
-	duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;
-	std::cout << "Read features pre" << " " << duration << std::endl;
-	
 	readFields(getFiles(getDirPath(baseDir, "features_postExc")), neuron_voxel_postExc, b2);
-
-	duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;
-	std::cout << "Read features post" << " " << duration << std::endl;
-
 	readMapFloat(getDirPath(baseDir, "features_postAll").append("\\voxel_postAllExc.dat"), voxel_postAllExc, b3);
 
-	duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;
-	std::cout << "Read data" << " " << duration << std::endl;
-
+	if (verbose) {
+		duration = (std::clock() - readFeatureTime) / (double)CLOCKS_PER_SEC;
+		std::cout << "[*] Read features " << duration << std::endl;
+	}
 	std::size_t nPre = neuron_voxel_pre.size();
 	std::size_t nPost = neuron_voxel_postExc.size();
 	std::size_t nVoxel = voxel_postAllExc.size();
 
-	float* pre = (float*)malloc(nPre * nVoxel * sizeof(float));
-	float* post = (float*)malloc(nPost * nVoxel * sizeof(float));
-	float* postAll = (float*)malloc(nVoxel * sizeof(float));
-
-	flattenFeatures(neuron_voxel_pre, neuron_voxel_postExc, voxel_postAllExc, pre, post, postAll);
-
-	duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;
-	std::cout << "Flattened data" << " " << duration << std::endl;
-
+	if (verbose) {
+		std::cout << "[*] Presynaptic: " << nPre << " Postsynaptic: " << nPost << " Voxels: " << nVoxel << std::endl;
+	}
 	std::random_device rd;
 	std::mt19937 randomGenerator(rd());
-
-	std::vector<int> empty(nPost, 0);
-	std::vector<std::vector<int> > contacts(nPre, empty);
 	std::uniform_real_distribution<float> dis(0.0, 1.0);
 
-	#pragma omp parallel for schedule(dynamic)
-	for (unsigned int i = 0; i < nPre; i++)
-	{
-		for (unsigned int j = 0; j < nPost; j++)
+	float* connections = (float*)malloc(nPre * nPost * sizeof(float));
+
+	for (int i = 0; i < nPre; i++) {
+		for (int j = 0; j < nPost; j++) {
+			connections[i * nPost + j] = 0;
+		}
+	}
+
+	if (!gpu) {
+
+		std::vector<int> preIndices;
+		std::vector<int> postIndices;
+
+		for (auto it = neuron_voxel_pre.begin(); it != neuron_voxel_pre.end(); ++it)
 		{
-			for (unsigned int k = 0; k < nVoxel; k++)
+			preIndices.push_back(it->first);
+		}
+
+		for (auto it = neuron_voxel_postExc.begin(); it != neuron_voxel_postExc.end(); ++it)
+		{
+			postIndices.push_back(it->first);
+		}
+
+		double computeCPUStartTime = std::clock();
+
+#pragma omp parallel for schedule(dynamic)
+		for (unsigned int i = 0; i < preIndices.size(); i++)
+		{
+			int preId = preIndices[i];
+			for (unsigned int j = 0; j < postIndices.size(); j++)
 			{
-				float preVal = pre[i * nVoxel + k];
-				float postVal = post[j * nVoxel + k];
-				float postAllVal = postAll[k];
-				if (preVal != 0 && postVal != 0) {
-					float arg = b0 + preVal + postVal + postAllVal;
-					int synapses = 0;
-					if (arg >= -7 && arg <= 7)
+				int postId = postIndices[j];
+				//qDebug() << i << j << preId << postId;
+				if (preId != postId)
+				{
+					for (auto pre = neuron_voxel_pre[preId].begin(); pre != neuron_voxel_pre[preId].end(); ++pre)
 					{
-						float mu = exp(arg);
-						float prob = 1 - exp(-1 * mu);
-						float rand = dis(randomGenerator);
-						synapses = rand <= prob ? 1 : 0;
-					}
-					else if (arg > 7)
-					{
-						synapses = 1;
-					}
-					if (synapses > 0)
-					{
-						contacts[i][j] = synapses;
-						break;
+						if (neuron_voxel_postExc[postId].find(pre->first) != neuron_voxel_postExc[postId].end())
+						{
+							float preVal = pre->second;
+							float postVal = neuron_voxel_postExc[postId][pre->first];
+							float postAllVal = voxel_postAllExc[pre->first];
+							float arg = b0 + preVal + postVal + postAllVal;
+							//int synapses = 0;
+							if (arg >= -7 && arg <= 7)
+							{
+								float mu = exp(arg);
+								connections[i * nPost + j] += mu;
+							}
+							else if (arg > 7)
+							{
+								connections[i * nPost + j] = 1000;
+								break;
+							}
+						}
 					}
 				}
 			}
 		}
+
+		if (verbose) {
+			duration = (std::clock() - computeCPUStartTime) / (double)CLOCKS_PER_SEC;
+			std::cout << "[*] Compute CPU " << duration << std::endl;
+		}
+	}
+	else
+	{
+		float* pre = (float*)malloc(nPre * nVoxel * sizeof(float));
+		float* post = (float*)malloc(nPost * nVoxel * sizeof(float));
+		float* postAll = (float*)malloc(nVoxel * sizeof(float));
+
+		double flattenFeaturesTime = std::clock();
+
+		flattenFeatures(neuron_voxel_pre, neuron_voxel_postExc, voxel_postAllExc, pre, post, postAll);
+
+		if (verbose) {
+			duration = (std::clock() - flattenFeaturesTime) / (double)CLOCKS_PER_SEC;
+			std::cout << "[*] Flatten features " << duration << std::endl;
+		}
+		cudaError_t cudaStatus = calcWithCuda(pre, post, postAll, b0, nPre, nPost, nVoxel, connections, start, verbose);
+
+		if (cudaStatus != cudaSuccess) {
+			fprintf(stderr, "calcWithCuda failed!");
+			return 1;
+		}
+
+		cudaStatus = cudaDeviceReset();
+		if (cudaStatus != cudaSuccess) {
+			fprintf(stderr, "cudaDeviceReset failed!");
+			return 1;
+		}
+
+		free(pre);
+		free(post);
+		free(postAll);
 	}
 
-	duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;
-	std::cout << "Finished synapse distribution" << " " << duration << std::endl;
+	double computeProbabilityStartTime = std::clock();
 
 	float connectionProbability = 0;
-
 	for (unsigned int i = 0; i < nPre; i++) {
 		int realizedConnections = 0;
 		for (unsigned int j = 0; j < nPost; j++) {
-			realizedConnections += contacts[i][j] > 0 ? 1 : 0;
+			float mu = connections[i * nPost + j];
+			if (mu <= 1000) {
+				float prob = 1 - exp(-1 * mu);
+				float rand = dis(randomGenerator);
+				realizedConnections += rand <= prob ? 1 : 0;
+			}
+			else {
+				realizedConnections++;
+			}
 		}
 		connectionProbability += (float)realizedConnections / (float)nPost;
 	}
 	connectionProbability /= (float)nPre;
+	free(connections);
+
+	duration = (std::clock() - computeProbabilityStartTime) / (double)CLOCKS_PER_SEC;
+	//std::cout << "[*] Compute connection probability " << duration << std::endl;
+
+	std::ofstream outfile;
+	outfile.open("output.json");
+	outfile << "{\"CONNECTION_PROBABILITY\":" << connectionProbability << "}";
+	outfile.close();
 
 	duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;
-	std::cout << "Computed connection probability" << " " << duration << std::endl;
+	std::cout << "[*] Finish simulation " << duration << "s" << std::endl;
+	std::cout << "[*] Connection prob. " << connectionProbability << std::endl;
 
-	/*
-	cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "addWithCuda failed!");
-		return 1;
-	*/
-
-	/*
-	cudaError_t cudaStatus = cudaDeviceReset();
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaDeviceReset failed!");
-		return 1;
-	}
-	*/
-
-	std::cout << "Finish simulation" << " " << b0 << " " << b1 << " " << b2 << " " << b3 << " prob. " << connectionProbability << std::endl;
-
-	free(pre);
-	free(post);
-	free(postAll);
 
 	return 0;
 }
